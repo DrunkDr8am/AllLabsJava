005730c6da29928ef918520c59dc1eac8a6984ba
DrunkDr8am <60920007+DrunkDr8am@users.noreply.github.com>
2022-05-28 01:16:41 +0300

    add teory

---
+++ src/main/java/head3/teory10/Dog.java
@@ -0,0 +1,13 @@
+package head3.teory10;
+
+public class Dog {
+    String name;
+    int age;
+    int weight;
+
+    public Dog(final String name, final int age, final int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+}
